7dca9663ee5ce61ba05ef19ac7881381f5254267
vighnes9 <96604633+vighnes9@users.noreply.github.com>
2022-02-11 15:39:58 +0530

    Add files via upload

---
+++ c98 project/swappingFile.py
@@ -0,0 +1,13 @@
+def swapFileData():
+    file1 = input("Enter File 1 Name :")
+    file2 = input("Enter File 2 Name :")
+    with open(file1,'r')as a:
+        data_a = a.read()
+    with open(file2,'r')as b:
+        data_b = b.read()
+    with open(file1,'w')as a:
+        a.write(data_b)
+    with open(file2,'w')as b:
+        b.write(data_a) 
+
+swapFileData()
